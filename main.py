323dda45f87773a1d35efbbb12eb9d10bde4f407
Milesq <wmilosz88@gmail.com>
2024-12-08 15:24:14 +0100

    recursive search git repos, use only non gh ones

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
+from typing import List
 import git
 from src.Importer import Importer
 import os
 
-def list_repos(parent_dir):
-    return [git.Repo(os.path.join(parent_dir, path)) for path in os.listdir(parent_dir) if os.path.isdir(os.path.join(parent_dir, path))]
+
+def find_git_repos(directory) -> List[git.Repo]:
+    git_repos = []
+    for root, dirs, _files in os.walk(directory, followlinks=True):
+        if '.git' in dirs:
+            git_repos.append(git.Repo(root))
+            dirs.remove('.git')
+
+    return git_repos
 
 
-repos = [
-    *list_repos("/home/milesq/Desktop/dev/current/bbcs"),
-    *list_repos("/home/milesq/Desktop/dev/current/uktv-compliance")
+repos = find_git_repos("/home/milesq/Desktop/dev/current/job")
+
+non_gh_repos = [
+    repo for repo in repos
+    if repo.remotes
+    and 'github' not in repo.remotes[0].url
 ]
 
 mock_repo = git.Repo("./other-git-servers-mock")
-
+mock_repo.remotes.append(git.Remote.add(mock_repo, "origin", "https://github.com/Milesq/other-git-servers-mock"))
 importer = Importer(repos, mock_repo)
 importer.set_author(['wmilosz88@gmail.com', 'milosz.wisniewski@ecreationmedia.tv'])
 importer.import_repository()
